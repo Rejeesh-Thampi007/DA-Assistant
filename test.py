71900998942ea3b4e50a0ed1a840551d51abdcd6
Sravan Pant <sravan.pant5@gmail.com>
2026-05-22 11:39:42 +0530

    fix: updated the test file to use env

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
-from langchain_openai import ChatOpenAI  
-import os  
-import httpx  
-client = httpx.Client(verify=False) 
-llm = ChatOpenAI( 
-    base_url="https://genailab.tcs.in",
-    model = "azure_ai/genailab-maas-DeepSeek-V3-0324", 
-    api_key="sk-uoDXudpMggVIu9LSwZF1eQ", # Will be provided during event.  And this key is for 
-    http_client = client 
-) 
-output = llm.invoke("Hi")
-print(output)
+import os
+import httpx
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+client = httpx.Client(verify=False)
+
+llm = ChatOpenAI(
+    base_url=os.getenv("BASE_URL"),
+    model="azure_ai/genailab-maas-DeepSeek-V3-0324",
+    api_key=os.getenv("API_KEY"),
+    http_client=client,
+)
+
+response = llm.invoke("Hi")
+print(response.content)
